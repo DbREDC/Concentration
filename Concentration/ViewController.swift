fef232fa4cbf4489ce7ed10002f478988d458217
孙鹏鹏 <775777812@qq.com>
2018-08-05 01:16:32 +0800

    随机生成按钮图标

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,16 +10,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var buttonIndex = [Int](0..<30)
+    var lastButton = 0
+    let buttonImage = ["👻","🎃","⌚️","🎁","🚗","🏀","🐶","🐤","😄","🍓","🍒","🍞","🍺","🎲","🎹"]
+    
+    
+    var flipCount = 0 {
+        didSet{
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBOutlet var cardButton: [UIButton]!
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBAction func touchButton(_ sender: UIButton) {
+        flipCount += 1
+        if let index = cardButton.index(of: sender){
+            let chooseImage = buttonIndex[index]%15
+            flipCard(withEmoji: buttonImage[chooseImage], on: sender)
+            lastButton = index
+        }
+    }
+    
+    func flipCard(withEmoji emoji: String,on button: UIButton){
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        }else{
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        if flipCount%2==0 {
+            if lastButton == cardButton.index(of: button){
+                button.removeFromSuperview()
+                cardButton[lastButton].removeFromSuperview()
+            }
+//                button.setTitle("", for: UIControlState.normal)
+//                button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+//                cardButton[lastButton].setTitle("", for: UIControlState.normal)
+//                cardButton[lastButton].backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0,
+        }
+    }
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        shuffleArray()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func shuffleArray() {
+        let count = buttonIndex.count
+        for i in 0..<count {
+            let j = Int(arc4random_uniform(UInt32(count-i))) + i
+            if i != j {
+                (buttonIndex[i], buttonIndex[j])=(buttonIndex[j], buttonIndex[i])
+            }
+        }
     }
-
-
+   
 }
 
